6535b1d20a1ba5ed5f7be0f74e83e50787ba8d1d
Elizander Aguila <elizanderaguila@gmail.com>
2025-10-13 12:36:41 +0800

    Midterm Exams 1st Sem

---
+++ 1st Sem/Midterms_1stSem.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Midterms_1stSem {
+    public static void main(String[] args) {
+        System.out.println("\nMidterms 1st Sem: Computer Programming 3\n");
+        Scanner scanner = new Scanner(System.in);
+
+        // Switch Case Statement that Computes employee's end-of year bonus
+        System.out.print("Employee Name   : ");
+        String employeeName = scanner.nextLine();
+        System.out.print("Employee Salary : $");
+        double salary = scanner.nextDouble();
+        System.out.print("Employee Rating : ");
+        int rating = scanner.nextInt();
+
+        // Calculate bonus based on rating
+        double bonus;
+        switch (rating) {
+            case 1:
+                bonus = 0.25;
+                break;
+            case 2:
+                bonus = 0.15;
+                break;
+            case 3:
+                bonus = 0.10;
+                break;
+            default:
+                bonus = 0;
+                System.out.println("Invalid rating. No bonus awarded.");
+        }
+
+        System.out.println("Employee Bonus  : $" + (salary * bonus));
+        System.out.println("\nProgrammer's Name: Elizander Aguila");
+    }
+}
